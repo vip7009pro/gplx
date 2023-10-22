9d7d79daa53e95e669dd083205a9f6bf084ec3e7
vip7009pro <hung1893@gmail.com>
2023-10-22 17:00:10 +0700

    them login screen

--- app/src/main/java/com/hajima/vip7009pro/doctiengviet/MainActivity.kt
+++ app/src/main/java/com/hajima/vip7009pro/doctiengviet/MainActivity.kt
@@ -3,29 +3,15 @@ package com.hajima.vip7009pro.doctiengviet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.hajima.vip7009pro.doctiengviet.components.Components
 import com.hajima.vip7009pro.doctiengviet.ui.theme.DocTiengVietTheme
-import com.hajima.vip7009pro.doctiengviet.Components.*;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,18 +20,12 @@ class MainActivity : ComponentActivity() {
             DocTiengVietTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = Color.Red) {
-                   Components().LoginScreen()
+                    Components().LoginScreen()
                 }
             }
         }
     }
 }
-
-@Composable
-fun CommonSpace() {
-    Spacer(modifier = Modifier.height(24.dp))
-}
-
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
--- app/src/main/java/com/hajima/vip7009pro/doctiengviet/components/Components.kt
+++ app/src/main/java/com/hajima/vip7009pro/doctiengviet/components/Components.kt
@@ -1,16 +1,16 @@
-package com.hajima.vip7009pro.doctiengviet
+package com.hajima.vip7009pro.doctiengviet.components
 
-import android.provider.CalendarContract
-import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
@@ -18,8 +18,8 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -29,8 +29,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -38,6 +41,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hajima.vip7009pro.doctiengviet.R
 import com.hajima.vip7009pro.doctiengviet.ui.theme.DocTiengVietTheme
 
 class Components {
@@ -49,18 +53,36 @@ class Components {
         var password by remember {
             mutableStateOf("")
         }
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.cmsv_background),
+                contentDescription = "CMS Logo",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .blur(6.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
 
-        Surface(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.White)
-                .padding(28.dp)
+                .alpha(0.6f)
+                .background(MaterialTheme.colorScheme.background)
+                .padding(10.dp)
+                .clip(
+                    CutCornerShape(
+                        topStart = 8.dp, topEnd = 16.dp, bottomStart = 16.dp, bottomEnd = 8.dp
+                    )
+                )
         ) {
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(28.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
             ) {
                 LoginHeader()
                 LoginField(username, password, onUserNameChange = {
@@ -68,7 +90,7 @@ class Components {
                 }, onPasswordChange = {
                     password = it
                 })
-                LoginFooter()
+                LoginFooter(onSignInClick = {}, onSignUpClick = {})
             }
 
         }
@@ -76,50 +98,67 @@ class Components {
 
     @Composable
     fun LoginHeader() {
-        Text(
-            text = "Welcome Back",
-            fontSize = 36.sp,
-            fontWeight = FontWeight.ExtraBold,
-            textAlign = TextAlign.Center
-        )
-        Text(text = "Login to use ERP", fontSize = 25.sp, fontWeight = FontWeight.ExtraBold)
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Welcome Back",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.Center
+            )
+            Text(text = "Login to use ERP", fontSize = 25.sp, fontWeight = FontWeight.ExtraBold)
+
+        }
+
     }
 
     @Composable
-    fun ColumnScope.LoginField(
+    fun LoginField(
         username: String,
         password: String,
         onUserNameChange: (String) -> Unit,
         onPasswordChange: (String) -> Unit
     ) {
-        CustomField(value = username,
-            label = "Username",
-            placeholder = "Enter your ID",
-            onValueChange = onUserNameChange,
-            leadingIcon = {
-                Icon(Icons.Default.AccountBox, contentDescription = "ERP ID")
-            })
-        Spacer(modifier = Modifier.height(8.dp))
-        CustomField(
-            value = password,
-            label = "Password",
-            placeholder = "Enter your password",
-            onValueChange = onPasswordChange,
-            leadingIcon = {
-                Icon(Icons.Default.Lock, contentDescription = "ERP PASSWORD")
-            },
-            visualTransformation = PasswordVisualTransformation()
-        )
-        TextButton(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.End)) {
-            Text(text = "Forgot password")
+        Column {
+            CustomField(value = username,
+                label = "Username",
+                placeholder = "Enter your ID",
+                onValueChange = onUserNameChange,
+                leadingIcon = {
+                    Icon(Icons.Default.AccountBox, contentDescription = "ERP ID")
+                })
+            Spacer(modifier = Modifier.height(8.dp))
+            CustomField(
+                value = password,
+                label = "Password",
+                placeholder = "Enter your password",
+                onValueChange = onPasswordChange,
+                leadingIcon = {
+                    Icon(Icons.Default.Lock, contentDescription = "ERP PASSWORD")
+                },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            TextButton(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.End)) {
+                Text(text = "Forgot password")
+            }
+
         }
+
     }
 
     @Composable
-    fun LoginFooter() {
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Sign in")
+    fun LoginFooter(
+        onSignInClick: () -> Unit, onSignUpClick: () -> Unit
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = onSignInClick, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Login")
+            }
+            TextButton(onClick = onSignUpClick) {
+                Text(text = "Don't have an account, click here")
+            }
+
         }
+
 
     }
 
@@ -144,6 +183,7 @@ class Components {
             placeholder = {
                 Text(text = placeholder)
             },
+            singleLine = true,
             visualTransformation = visualTransformation,
             keyboardOptions = keyboardOptions,
             leadingIcon = leadingIcon,
