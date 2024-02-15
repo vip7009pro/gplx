b6da48bf22c8a4fe4504f0a0f8113b6b67ddfe16
vip7009pro <hung1893@gmail.com>
2024-02-15 14:12:33 +0700

    update dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
   buildTypes {
     release {
-      isMinifyEnabled = false
+      isMinifyEnabled = true
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
@@ -50,9 +50,9 @@ android {
 
 dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-  implementation("androidx.activity:activity-compose:1.8.1")
-  implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+  implementation("androidx.activity:activity-compose:1.8.2")
+  implementation(platform("androidx.compose:compose-bom:2024.02.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
@@ -60,13 +60,14 @@ dependencies {
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
   implementation("com.squareup.retrofit2:converter-gson:2.9.0")
   implementation ("com.github.farhanroy:compose-awesome-dialog:1.0.1")
-  implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+  implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
   implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-  implementation("androidx.navigation:navigation-compose:2.7.5")
+  implementation("androidx.navigation:navigation-compose:2.7.7")
   implementation ("com.github.Gurupreet:FontAwesomeCompose:1.1.0")
   implementation("io.coil-kt:coil-compose:2.5.0")
-  implementation ("com.google.dagger:hilt-android:2.49")
-  kapt ("com.google.dagger:hilt-android-compiler:2.49")
+  implementation ("com.google.dagger:hilt-android:2.50")
+  implementation("androidx.sqlite:sqlite-ktx:2.4.0")
+  kapt ("com.google.dagger:hilt-android-compiler:2.50")
   implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
   kapt ("androidx.hilt:hilt-compiler:1.1.0")
   implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
