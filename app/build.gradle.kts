ae113134562eb328f27762fe6ad6c12023e3714e
vip7009pro <hung1893@gmail.com>
2024-04-30 11:38:12 +0700

    update code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,6 @@ dependencies {
   kapt("androidx.room:room-compiler:2.6.1")
   implementation("androidx.room:room-ktx:2.6.1")
   testImplementation("androidx.room:room-testing:2.6.1")
-
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
